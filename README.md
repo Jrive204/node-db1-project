dfb302a59bf2a476508d1acefd1e502cd878163f
Jose <Jrivera6869@yahoo.com>
2020-02-18 00:12:18 -0500

    finished everything

--- README.md
+++ README.md
@@ -49,7 +49,9 @@ The following exercises **require research**, the concepts needed to complete th
 
 ```js
 // sample req.query object
-{
+
+ 
+{ 
   limit: 5,
   sortby: 'id',
   sortdir: 'desc'
--- server.js
+++ server.js
@@ -1,9 +1,61 @@
 const express = require('express');
 
 const db = require('./data/dbConfig.js');
+const query = require('./query');
 
 const server = express();
 
 server.use(express.json());
+
+server.get('/api/accounts', (req, res) => {
+  const { limit, sortby, sortdir } = req.query;
+  console.log(req.query, 'HELLO');
+
+  query
+    .getstuff({ limit, sortby, sortdir })
+    .then(
+      accounts => res.status(200).json(accounts) & console.log(accounts.length)
+    )
+    .catch(err => res.status(500).json(err.message));
+});
+
+server.get('/api/accounts/:id', (req, res) => {
+  const { id } = req.params;
+  db('accounts')
+    .where({ id })
+    .first()
+    .then(account => res.status(200).json(account))
+    .catch(err => res.status(500).json(err.message));
+});
+//  POST --------
+
+server.post('/api/accounts', (req, res) => {
+  db('accounts')
+    .insert(req.body, 'id')
+    .then(account => res.status(201).json(account))
+    .catch(err => res.status(500).json(err.message));
+});
+
+//  PUT -----------------------
+
+server.put('/api/accounts/:id', (req, res) => {
+  const { id } = req.params;
+  db('accounts')
+    .where({ id })
+    .update(req.body)
+    .then(account => res.status(200).json(account))
+    .catch(err => res.status(500).json(err.message));
+});
+
+// DELETE  ---------
+
+server.delete('/api/accounts/:id', (req, res) => {
+  const { id } = req.params;
+  db('accounts')
+    .where({ id })
+    .delete()
+    .then(account => res.status(200).json(account))
+    .catch(err => res.status(500).json(err.message));
+});
 
 module.exports = server;
